c084eb08acdf9758346a4bb0be4f0d7cc0079f87
Shayne Holmes <github.snth@xoxy.net>
2023-12-02 11:25:05 -0800

    Tighten up makefile and upgrade template

--- 2023/02/part1.go
+++ 2023/02/part1.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func atoi(s string) int {
@@ -23,13 +24,40 @@ func main() {
 	}
 	defer file.Close()
 
+	cubeCountbyColor := map[string]int{
+		"blue":  14,
+		"red":   12,
+		"green": 13,
+	}
+
+	possibleGamesSum := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
+		game := scanner.Text()
+		gameSplit := strings.Split(game, ": ")
+		gameId, reveals := atoi(strings.Split(gameSplit[0], " ")[1]), strings.Split(gameSplit[1], "; ")
+
+		feasible := true
+		for ri, r := range reveals {
+			colorsWithCounts := strings.Split(r, ", ")
+			for ci, c := range colorsWithCounts {
+				split := strings.Split(c, " ")
+				count, color := atoi(split[0]), split[1]
+				fmt.Printf("game %3d %3d[%3d]: %d %s <> %d\n", gameId, ri, ci, count, color, cubeCountbyColor[color])
+				if count > cubeCountbyColor[color] {
+					feasible = false
+					break
+				}
+			}
+		}
+
+		if feasible {
+			possibleGamesSum += gameId
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(possibleGamesSum)
 }
--- 2023/template.go
+++ 2023/template.go
@@ -8,14 +8,6 @@ import (
 	"strconv"
 )
 
-func atoi(s string) int {
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		log.Fatalf("not an int: %q", s)
-	}
-	return i
-}
-
 func main() {
 	file, err := os.Open("./input.txt")
 	if err != nil {
@@ -32,4 +24,80 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// cheap atoi
+func atoi(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatalf("not an int: %q", s)
+	}
+	return i
+}
+
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcd(b, a%b)
+}
+
+func lcm(a, b int) int {
+	return a / gcd(a, b) * b
+}
+
+func multiply(values ...int) int {
+	if len(values) == 0 {
+		log.Fatalf("no value in sum function")
+	}
+
+	product := 1
+	for _, value := range values {
+		product *= value
+	}
+	return product
+}
+
+func sum(values ...int) int {
+	if len(values) == 0 {
+		panic("no value in sum function")
+	}
+
+	sum := 0
+	for _, value := range values {
+		sum += value
+	}
+	return sum
+}
+
+type nullableInt struct {
+	value int
+}
+
+func max(values ...int) int {
+	if len(values) == 0 {
+		panic("no value in max function")
+	}
+
+	var max *nullableInt
+	for _, value := range values {
+		if max == nil || value > max.value {
+			max = &nullableInt{value}
+		}
+	}
+	return max.value
+}
+
+func min(values ...int) int {
+	if len(values) == 0 {
+		panic("no value in min function")
+	}
+
+	var min *nullableInt
+	for _, value := range values {
+		if min == nil || value < min.value {
+			min = &nullableInt{value}
+		}
+	}
+	return min.value
 }
